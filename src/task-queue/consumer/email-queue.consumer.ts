a7dd444335f708cf361b5860b9597e89f648b970
dulanwirajith <dulanwirajith1995@gmail.com>
2023-04-08 01:25:40 +0530

    feat(store data): data store in on event completed done

--- src/task-queue/consumer/email-queue.consumer.ts
+++ src/task-queue/consumer/email-queue.consumer.ts
@@ -56,31 +56,44 @@ export class EmailQueueConsumer {
         },
       });
 
-      await this.prisma.email.update({
-        where: {
-          id: emailRes.id,
-        },
-        data: {
-          logs: {
-            connect: {
-              id: logRes.id,
-            },
-          },
-        },
-      });
+      await this.makeConnection(emailRes.id, logRes.id);
     } catch (e) {
       throw new BadRequestException(e);
     }
   }
 
   @OnQueueEvent(BullQueueEvents.COMPLETED)
-  onCompleted(job: Job) {
-    this.logger.warn(
-      `Completed - Email Queue ID ${job.id} with RESULT: ${
-        job.returnvalue
-      } and DATA: ${JSON.stringify(job)}`,
-    );
-    // todo :when Event Success
+  async onCompleted(job: Job) {
+    try {
+      this.logger.warn(
+        `Completed - Email Queue ID ${job.id} with RESULT: ${
+          job.returnvalue
+        } and DATA: ${JSON.stringify(job)}`,
+      );
+      const { externalId } = job.data;
+      const emailRes = await this.prisma.email.findUnique({
+        where: {
+          externalId,
+        },
+        select: {
+          id: true,
+        },
+      });
+
+      const logRes = await this.prisma.taskQueueLog.create({
+        data: {
+          queueName: EMAIL_QUEUE,
+          jobId: job.id.toString(),
+          jobData: job.data,
+          job: job as any,
+          status: EmailStatusEnum.SUCCESS,
+        },
+      });
+
+      await this.makeConnection(emailRes.id, logRes.id);
+    } catch (e) {
+      throw new BadRequestException(e);
+    }
   }
 
   @OnQueueEvent(BullQueueEvents.FAILED)
@@ -91,5 +104,20 @@ export class EmailQueueConsumer {
       } and DATA: ${JSON.stringify(job)}`,
     );
     // todo :when Event Failed
+  }
+
+  async makeConnection(emailId: string, logId: string) {
+    await this.prisma.email.update({
+      where: {
+        id: emailId,
+      },
+      data: {
+        logs: {
+          connect: {
+            id: logId,
+          },
+        },
+      },
+    });
   }
 }

301b0a5c62ba03bb9c19592f2118674dc19bbd9e
dulanwirajith <dulanwirajith1995@gmail.com>
2023-04-09 21:58:20 +0530

    feat(rabbitmq integration): communication and remove from queue done

--- apps/pickles-email-service/src/app.controller.ts
+++ apps/pickles-email-service/src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { Ctx, EventPattern, Payload, RmqContext } from '@nestjs/microservices';
+import { RmqService } from '@app/common/rmq/rmq.service';
 import { AppService } from './app.service';
 import { MailSendDto } from './mail/dto/mail-send.dto';
 import { MailService } from './mail/mail.service';
@@ -9,6 +10,7 @@ export class AppController {
   constructor(
     private readonly appService: AppService,
     private readonly mailService: MailService,
+    private readonly rmqService: RmqService,
   ) {}
 
   @Get('health')
@@ -21,9 +23,8 @@ export class AppController {
     @Payload() data: MailSendDto,
     @Ctx() context: RmqContext,
   ) {
+    console.log('data in handleNewEmailSendRequest', data);
     this.mailService.handleMailSend(data);
-    console.log('hey');
-    console.log(data);
-    console.log(context);
+    this.rmqService.ack(context);
   }
 }
--- apps/users/src/users.service.ts
+++ apps/users/src/users.service.ts
@@ -44,6 +44,5 @@ export class UsersService {
         companyName: 'CodeWithDulan',
       },
     });
-    return newUser;
   }
 }
